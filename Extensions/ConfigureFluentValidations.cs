fc691a44c9744238e9926bbe21a78d0d14bf8a2a
DeraE2024 <careers.joseph@yahoo.com>
2024-08-07 04:46:49 +0100

    add fluent validation

---
+++ Extensions/ConfigureFluentValidations.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using gateway.api.FluentValidations;
+
+namespace gateway.api.Extensions
+{
+    public static class ConfigureFluentValidations
+    {
+        public static void InjectFluentValidations(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssemblyContaining<LoginDtoValidator>();
+            services.AddValidatorsFromAssemblyContaining<RefreshTokenRequestDtoValidator>();
+
+            services.AddFluentValidationAutoValidation(options =>
+            {
+                options.DisableDataAnnotationsValidation = true;
+            });
+            services.AddFluentValidationClientsideAdapters();
+        }
+    }
+}
---
+++ FluentValidations/RefreshTokenRequestDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using gateway.api.Models.Token;
+
+namespace gateway.api.FluentValidations
+{
+    public class RefreshTokenRequestDtoValidator : AbstractValidator<RefreshTokenRequestDto>
+    {
+        public RefreshTokenRequestDtoValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("Please enter a valid user ID")
+                .NotNull().WithMessage("Please enter a valid user ID");
+
+            RuleFor(x => x.RefreshToken)
+                .NotEqual(Guid.Empty);
+        }
+    }
+}
